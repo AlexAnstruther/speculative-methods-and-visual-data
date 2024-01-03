8c4f5b276f8c5891653a4a9d1f619bc2c4329ad6
AlexAnstruther <114929753+AlexAnstruther@users.noreply.github.com>
2024-01-03 12:39:39 +0000

    Delete scripts/.ipynb_checkpoints directory

--- scripts/.ipynb_checkpoints/data-checkpoint.py
+++
@@ -1,3 +0,0 @@
-ratings_df = pd.read_csv('archive/ratings_export.csv')
-movies_df = pd.read_csv('archive/movie_data.csv',lineterminator='\n')
-print('All data successfully imported. Your datframes are: \'ratings_df\' and \'movies_df\'')
